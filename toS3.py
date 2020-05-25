124fad6906d9a00ca0a699ab46865f5541a12f2c
csu <imcssu@gmail.com>
2020-05-24 22:31:30 -0700

    boto3 unco

--- toS3.py
+++ toS3.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-# import boto3
+import boto3
 
 app = Flask(__name__)
 
@@ -12,9 +12,9 @@ def index():
 
 @app.route('/upload', methods = ['POST'])
 def upload():
-	# s3 = boto3.resource('s3')
+	s3 = boto3.resource('s3')
 
-	# s3.Bucket('prettyprinted').put_object(Key = 'a_python_file.py', Body=request.files['myfile'])
+	s3.Bucket('prettyprinted').put_object(Key = 'a_python_file.py', Body=request.files['myfile'])
 
 	return '<h1>File saved to S3>/h1'
 
