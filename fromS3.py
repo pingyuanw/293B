8b3c04dcfaf04aed4453723be836464bf4a19322
csu <imcssu@gmail.com>
2020-05-25 17:24:23 -0700

    check if file updated with ok search and is able to deal with model not found

--- fromS3.py
+++ fromS3.py
@@ -11,12 +11,18 @@ def downloadFromS3(buc_name,filename,dest_name):
 # this returns the last modified date of the model
 # we want to use this output in compareDate to see if that is the most 
 # recently used file
+# if coulndt locate model, return a date in 2000, and print Does not find
 def getLastModified(buc_name):
 	s3 = boto3.client('s3')
 	listOfFiles = s3.list_objects_v2(Bucket='hummingbird-293')['Contents']
 	# need a proper search alg, implemented later on
-	modelfile = listOfFiles[1]
-	lastModified = modelfile['LastModified']
+	index = findModelfiles(listOfFiles)
+	if index > -1:
+		modelfile = listOfFiles[index]
+		lastModified = modelfile['LastModified']
+	else:
+		print("Does not find model")
+		return datetime.datetime(2000,1,1,0,0,0,0,tzinfo=datetime.timezone.utc)
 
 	return lastModified
 
@@ -24,12 +30,29 @@ def getLastModified(buc_name):
 # if they are not equal that means, we need a update of model
 # return true means we need an update
 def compareDate(original_date, current_date):
-	return not(original_date==current_date)
+	# check if the crrent_date is a valid date
+	if current_date == datetime.datetime(2000,1,1,0,0,0,0,tzinfo=datetime.timezone.utc):
+		print("File not found")
+		return False
+	return (original_date<current_date)
 
-# an helper method that is used to find the file of model
+# A helper method that is used to find the file of model
+# returns the index of corresponding file
+# if index = -1 that means not found
 def findModelfiles(listOfFiles):
+	index = 0
+	for e in listOfFiles:
+		if e['Key']=='Models/resnet50_ckpt.pth':
+			return index
+		index = index + 1
+	index = -1
+	return index
 
 
+# below were used to test the function
 # print(getLastModified('hummingbird-293'))
+# test = datetime.datetime(2020,5,15,7,38,25,0,tzinfo=datetime.timezone.utc)
+# print(test)
+# print(compareDate(test,getLastModified('hummingbird-293')))
 
 
