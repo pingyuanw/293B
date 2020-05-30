d6f1ddff86f9cd75dc90fcf59dc1e428889a0d9c
csu <imcssu@gmail.com>
2020-05-30 11:21:01 -0700

    toS3 update

--- toS3.py
+++ toS3.py
@@ -1,22 +1,13 @@
-from flask import Flask, request
 import boto3
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return '''<form method=POST enctype=multipart/form-data action="upload">
-	<input type=file name=myfile>
-	<input type = submit>
-	</form>'''
-
-@app.route('/upload', methods = ['POST'])
-def upload():
+# filename need to include the path
+# Key = dest file in the cloud,  body = src file in the edge
+def upload(filename,destfile):
 	s3 = boto3.resource('s3')
 
-	s3.Bucket('hummingbird-293').put_object(Key = 'edgeTeam/dog.png', Body=request.files['myfile'])
+	s3.Bucket('hummingbird-293').put_object(Key = filename, Body=destfile)
 
-	return '<h1>File saved to S3>/h1'
 
-if __name__ == '__main__':
-	app.run(debug=True)
+# example function called for upload
+# upload('edgeTeam/dog-05-26-2020.png','anonymized/dog-05-26-2020,12/21/14.png')
